de20ae5e9855fe886d0533d602d4241160af63c7
neagualexa <ngu.alexandra@gmail.com>
2025-09-04 19:23:57 +0100

    add project docs

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ImageGallery from "./ImageGallery";
+import GithubIcon from "../data/misc/Github_icon.png";
+
+// Import all PDFs from the projects docs folder
+const importAll = (r) => {
+  let files = {};
+  r.keys().forEach((item) => {
+    files[item.replace("./", "")] = r(item);
+  });
+  return files;
+};
+
+const projectPDFs = importAll(
+  require.context("../data/misc/projects/docs", false, /\.(pdf)$/)
+);
 
 const ProjectCard = ({ project }) => {
   return (
@@ -36,10 +50,52 @@ const ProjectCard = ({ project }) => {
               target="_blank"
               rel="noopener noreferrer"
               className="project-link"
+              style={{
+                display: "inline-flex",
+                alignItems: "center",
+                gap: "0.5rem",
+                marginRight: "1rem",
+                marginBottom: "0.5rem",
+              }}
             >
+              {link.type === "github" && (
+                <img
+                  src={GithubIcon}
+                  alt="GitHub icon"
+                  style={{ width: "16px", height: "16px" }}
+                />
+              )}
               {link.text}
             </a>
           ))}
+        </div>
+      )}
+
+      {project.resources && project.resources.length > 0 && (
+        <div className="project-resources">
+          <strong>Resources:</strong>
+          <div style={{ marginTop: "0.5rem" }}>
+            {project.resources.map((resource, index) => {
+              let href = resource.url;
+
+              // If we have a filename that matches imported PDFs, use the imported PDF
+              if (resource.url && projectPDFs[resource.url]) {
+                href = projectPDFs[resource.url];
+              }
+
+              return (
+                <a
+                  key={index}
+                  href={href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="publication-link"
+                >
+                  {resource.text}
+                </a>
+              );
+            })}
+          </div>
         </div>
       )}
     </div>
@@ -54,6 +110,13 @@ ProjectCard.propTypes = {
     images: PropTypes.arrayOf(PropTypes.string),
     videos: PropTypes.arrayOf(PropTypes.string),
     links: PropTypes.arrayOf(
+      PropTypes.shape({
+        text: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        type: PropTypes.string,
+      })
+    ),
+    resources: PropTypes.arrayOf(
       PropTypes.shape({
         text: PropTypes.string.isRequired,
         url: PropTypes.string.isRequired,
