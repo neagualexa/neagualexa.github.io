2944cac11e0ace6128446ddcdaf646dc2da852e1
neagualexa <ngu.alexandra@gmail.com>
2025-09-09 16:34:12 +0100

    generalised styles + icons

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -1,7 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import GitHubIcon from "../data/misc/Github_icon.png";
+import LinkedInIcon from "../data/misc/LinkedIn_icon.png";
 
 const SocialLink = ({ name, url, display }) => {
+  // Function to get the appropriate icon based on the social platform
+  const getIcon = (socialName) => {
+    const lowerName = socialName.toLowerCase();
+    if (lowerName.includes("github")) {
+      return GitHubIcon;
+    } else if (lowerName.includes("linkedin")) {
+      return LinkedInIcon;
+    }
+    return null;
+  };
+
+  const icon = getIcon(name);
+
   return (
     <a
       href={url}
@@ -9,6 +24,13 @@ const SocialLink = ({ name, url, display }) => {
       rel="noopener noreferrer"
       className="social-link"
     >
+      {icon && (
+        <img
+          src={icon}
+          alt={`${name} icon`}
+          style={{ width: "16px", height: "16px", marginRight: "0.5rem" }}
+        />
+      )}
       <span>{display || name}</span>
     </a>
   );
